7304998f60d72071a0467bf4d2b946519263b3a0
Mauriciokr5 <mauricio_noble6@hotmail.com>
2022-11-13 21:13:37 -0600

    Clase catalogo terminada

---
+++ src/main/java/com/mycompany/practica2/Catalogo.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author Mauricio Beltrán
+ */
+public class Catalogo {
+    
+    ArrayList<Cancion> Canciones;
+    
+    public Catalogo() throws FileNotFoundException, IOException, ParseException{
+        this.Canciones =  new ArrayList<Cancion>();
+        //Se carga el json que tiene los ID y sus respectivos precios
+        JSONObject json = (JSONObject) (new JSONParser()).parse(new FileReader("precios.json"));
+        JSONArray precios = (JSONArray) json.get("PreciosCanciones");
+        
+        //Se carga los mp3 que estan en la carpeta catalogo
+        File catalogoPath = new File("catalogo");
+        File[] listaCanciones = catalogoPath.listFiles();
+        
+        Iterator<JSONObject> iterator = precios.iterator();
+        int i=0;
+        while (iterator.hasNext()) {
+            JSONObject var = iterator.next();
+            //En el orden de aparicion de los archivos se les va asignando el precio y el id conforme aparezca en el json
+            Canciones.add(new Cancion(listaCanciones[i], (double)var.get("precio"),((Long)var.get("id")).intValue()));
+            i++;
+        }
+    }
+    
+    
+    public void mostrarCatalogo(){
+        String formatoPrint = "| %-4d | %-30s | %-20s | %-25s | %-5s | %-8s | %-10s |%n";
+
+        System.out.format("+------+--------------------------------+----------------------+---------------------------+-------+----------+------------+%n");
+        System.out.format("| ID   | Nombre                         |      Autor           |          Album            |  Ano  | Duracion |   Precio   |%n");
+        System.out.format("+------+--------------------------------+----------------------+---------------------------+-------+----------+------------+%n");
+        
+        
+        for (Cancion c : Canciones) {
+            System.out.format(formatoPrint, c.id,c.nombre, c.autor, c.album, c.ano, c.duracion, "$"+c.precio);
+            System.out.format("+------+--------------------------------+----------------------+---------------------------+-------+----------+------------+%n");
+        }
+        
+    }
+    
+    
+    
+}
+
+
--- src/main/java/com/mycompany/practica2/Practica2.java
+++ src/main/java/com/mycompany/practica2/Practica2.java
@@ -6,6 +6,14 @@
 package com.mycompany.practica2;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -13,14 +21,38 @@ import java.io.File;
  */
 public class Practica2 {
 
-    public static void main(String[] args) {
-        File prueba = new File("catalogo\\Sparks.mp3");
-        Cancion cancion= new Cancion(prueba,1,350);
+    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+        //File prueba = new File("catalogo\\Sparks.mp3");
+        
+        Catalogo cat = new Catalogo();
+        cat.mostrarCatalogo();
+
+        /*
+        JSONObject json = (JSONObject) (new JSONParser()).parse(new FileReader("precios.json"));
+        JSONArray precios = (JSONArray) json.get("PreciosCanciones");
+        
+        Iterator<JSONObject> iterator = precios.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next().get("precio"));
+        }*/
+
+        /*String city = (String) person.get("city");
+        System.out.println(city);
+
+        String job = (String) person.get("job");
+        System.out.println(job);
+
+        JSONArray cars = (JSONArray) person.get("cars");*/
+
+         
+        
+        
+        /*Cancion cancion= new Cancion(prueba,1,350);
         System.out.println(cancion.album);
         System.out.println(cancion.ano);
         System.out.println(cancion.autor);
         System.out.println(cancion.duracion);
         System.out.println(cancion.nombre);
-        System.out.println("This is my pull");
+        System.out.println("This is my pull");*/
     }
 }
